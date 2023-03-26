dcbb35c6625cdb4d8a00fdc8f84faa5a8a50764e
Phung Minh Tuan Anh <tuananhphungminh1212@gmail.com>
2023-03-26 17:00:31 +0700

    first commit

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 HOST = 'http://localhost:5000' # Địa chỉ server trọng tài
-game_info = {}
-stop_thread = False
+game_info = {} # Thông tin trò chơi để hiển thị trên giao diện
+stop_thread = False # Biến dùng để dừng thread lắng nghe
 
-ai = TicTacToeAI('X')
+ai = TicTacToeAI('X') # Khởi tạo AI chạy với đội X
 
 class GameClient:
     def __init__(self, server_url):
@@ -30,19 +30,23 @@ class GameClient:
         # Lắng nghe yêu cầu từ server trọng tài
         # và cập nhật thông tin trò chơi
         while not stop_thread:
+            # Thời gian lắng nghe giữa các lần
             time.sleep(3)
             print(self.init)
+
+            # Nếu chưa kết nối thì gửi yêu cầu kết nối
             if not self.init:
                 response = requests.get(self.server_url + "/init")
             else:
                 response = requests.post(self.server_url)
             
+            # Lấy thông tin trò chơi từ server trọng tài và cập nhật vào game_info
             data = json.loads(response.content)
             global game_info
             game_info = data.copy()
 
+            # Khởi tạo trò chơi
             if data.get("init"):
-                #có thể input game match_id
                 self.init = True
                 print("Connection established")
                 print(data)
@@ -50,24 +54,30 @@ class GameClient:
                 self.init = True      
                 self.send_game_info()
                 
+            # Nhận thông tin trò chơi
             elif data.get("board"):
-                # Kiểm tra nước đi hợp lệ trước khi gửi yêu cầu
+                # Nếu là lượt đi của đội của mình thì gửi nước đi
                 if data.get("turn") == self.team_id:
                     self.size = int(data.get("size"))   
                     self.board = data["board"]
+                    # Lấy nước đi từ AI
                     move = ai.get_move(data.get("board"), data.get("size"))
                     print("Move: ", move)
+                    # Kiểm tra nước đi hợp lệ
                     valid_move = self.check_valid_move(move)
+                    # Nếu hợp lệ thì gửi nước đi
                     if valid_move:
                         self.board[int(move[0]) * self.size + int(move[1])] = "X"
                         self.send_move()
                     else:
                         print("Invalid move")
                     
+            # Kết thúc trò chơi
             elif data.get("status"):
                 print("Game over")
                 break
         
+    # Gửi thông tin trò chơi đến server trọng tài
     def send_game_info(self):
         headers = {"Content-Type": "application/json"}
         requests.post(self.server_url, json=game_info, headers=headers)
@@ -90,6 +100,7 @@ class GameClient:
             return True
         return False
 
+# API trả về thông tin trò chơi cho frontend
 @app.route('/')
 @cross_origin()
 def get_data():
@@ -99,12 +110,13 @@ def get_data():
 
 
 if __name__ == "__main__":
-    # HOST = input("Enter server url: ")
+    # Lấy địa chỉ server trọng tài từ người dùng
+    HOST = input("Enter server url: ")
+
+    # Khởi tạo game client
     gameClient = GameClient(HOST)
     game_thread = Thread(target=gameClient.listen)
     game_thread.start()
-    # gameClient.listen()
-    # print('hello')
     app.run(host="0.0.0.0", port="3005")    
     try:
         while game_thread.is_alive():
